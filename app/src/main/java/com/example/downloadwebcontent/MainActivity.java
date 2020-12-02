57796119318a67fbdc03be4d417a574817c3314d
@revoltcode <oddybassey@gmail.com>
2020-12-02 03:02:14 +0100

    first commit

---
+++ app/src/main/java/com/example/downloadwebcontent/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.downloadwebcontent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection httpURLConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.connect();
+                InputStream in = httpURLConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while(data != -1){
+                    char current = (char) data;
+                    result += current;
+
+                    data = reader.read();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                return "Failed!";
+            }
+            return result;
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+
+        try {
+            result = task.execute("http://www.zappycode.com").get();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        Log.i("Source Text Result--", result);
+    }
+}
